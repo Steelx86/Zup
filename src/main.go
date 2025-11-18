dfb4957a5735b4b920641c66564429b09039d4df
dlambda <20195teel@gmail.com>
2025-11-18 09:51:20 -0500

    Some work here

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"crypto/rand"
 )
 
 func main() {
@@ -12,10 +13,8 @@ func main() {
 		fileDir := os.Args[2]
 		key := os.Args[3]
 
-		file, err := os.Open(fileDir)
-
-		if err != nil {
-			fmt.Errorf("File failed to open: %v", err)
+		file, err := os.Open(fileDir); if err != nil {
+			fmt.Errorf("File failed to open: ", err)
 			return
 		}
 
@@ -30,4 +29,14 @@ func main() {
 		fmt.Print(helpMsg)
 	}
 
+}
+
+func genKey() ([]byte, error) {
+	key := make([]byte, 32)
+
+	_, err := rand.Read(key); if err != nil {
+		return nil, err
+	}
+
+	return key, err
 }
